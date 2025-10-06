626e0f5b4b12f22e88ba36ff96f7c0a1850a6e27
shah <shahrokh.r1379@gmail.com>
2025-10-06 18:52:33 +0330

    Implement shopping cart functionality

--- pages/product/[slug].js
+++ pages/product/[slug].js
@@ -9,6 +9,7 @@ import React, { useContext } from "react";
 export default function ProductDetail() {
   const { state, dispatch } = useContext(Store);
 
+  const router = useRouter();
   const { query } = useRouter();
   const { slug } = query;
   const product = data.products.find((item) => item.slug === slug);
@@ -31,6 +32,8 @@ export default function ProductDetail() {
       type: "CART_ADD_ITEM",
       payload: { ...product, quantity: quantity },
     });
+
+    router.push("/cart");
   };
 
   return (
